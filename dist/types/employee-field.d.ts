ef4e917eeb12abbaba10336dc77f419b9c757193
Zahid Allaulddin <zahidalaaudin@gmail.com>
2025-01-20 10:50:41 +0300

    add types for employee field config

--- dist/types/employee-field.d.ts
+++ dist/types/employee-field.d.ts
@@ -1,17 +1,46 @@
+export enum EmployeeFieldRecruitmentGroup {
+	PERSONAL = "personalInformation",
+	EMPLOYMENT = "employmentInformation",
+	PASSPORT = "passportInformation",
+	NATIONAL_CARD = "nationalCardInformation",
+	VISA = "visaInformation",
+	MEDICAL = "medicalInformation",
+	OTHER = "otherInformation",
+}
+
+type EmployeeFieldBase = {
+	field: string;
+	fieldType: "STRING" | "NUMBER" | "DATE" | "BOOLEAN" | "DATETIME" | "TIME";
+	defaultValue?: any;
+};
+
+type EmployeeFieldRequest = {
+	mode?: "LATEST" | "ALL" | "SUM" | "COUNT" | "EXISTENCE";
+	requestType?: string;
+	requestCategory?: string;
+};
+
+type EmployeeFieldRecruitment = {
+	recruitmentGroup?: EmployeeFieldRecruitmentGroup;
+};
+
+type EmployeeFieldRequestInput = EmployeeFieldBase & EmployeeFieldRequest;
+type EmployeeFieldRecruitmentInput = EmployeeFieldBase & EmployeeFieldRecruitment;
+
+type EmployeeFieldInput = EmployeeFieldRequestInput | EmployeeFieldRecruitmentInput;
+
 export interface IEmployeeField {
 	id: string;
 	createdAt: string;
 	updatedAt?: string;
 	regionId: string;
 
-	employeeId: string;
+	employeeId?: string;
 
 	name: string;
-	category: string;
+	category?: string;
 	type: "METADATA" | "REQUEST" | "RECRUITMENT";
-	config: {
-		[keyof: PropertyKey]: any;
-	};
+	config?: EmployeeFieldInput;
 
-	fallbackId: string;
+	fallbackId?: string;
 }
