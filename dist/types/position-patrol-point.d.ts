108f3b7255a2971c85de9cd08d4109f98f2527c0
Ali AlQassab <alqasssab23@gmail.com>
2025-07-14 10:15:57 +0300

    feat: add reportTypeId and reportCategoryId fields to IPositionPatrolPoint interface

--- dist/types/position-patrol-point.d.ts
+++ dist/types/position-patrol-point.d.ts
@@ -7,7 +7,10 @@ export interface IPositionPatrolPoint {
 	description?: string;
 	order: number;
 	type: "CLOSE_ENDED_QUESTION" | "OPEN_ENDED_QUESTION" | "NO_ACTION" | "PHOTO" | "VIDEO";
+	reportNotesTrigger?: string;
 
 	patrolId: string;
 	nfcTagId?: string;
+	reportTypeId?: string;
+	reportCategoryId?: string;
 }
