e410beb3b6d638a57d401e33453f0402c02ee456
Ali AlQassab <alqasssab23@gmail.com>
2025-08-18 11:39:58 +0300

    feat: add position template and position template item interfaces

---
+++ dist/types/position-template-item.d.ts
@@ -0,0 +1,13 @@
+export interface IPositionTemplateItem {
+	id: string;
+	createdAt: string;
+	updatedAt: string;
+
+	order: number;
+
+	shiftTypeId: string;
+
+	positionTemplateId: string;
+
+	metadata: any;
+}
---
+++ dist/types/position-template.d.ts
@@ -0,0 +1,10 @@
+export interface IPositionTemplate {
+	id: string;
+	createdAt: string;
+	updatedAt: string;
+
+	regionId: string;
+	hours: number;
+
+	metadata: any;
+}
