a854db7abd702018d40e07bc285cc0fa2aea0e8d
Ali AlQassab <alqasssab23@gmail.com>
2026-03-03 06:39:05 +0300

    feat: add optional darkColors and lightColors properties to IClientStatus interface

--- dist/types/client-status.d.ts
+++ dist/types/client-status.d.ts
@@ -1,3 +1,5 @@
+import { ITagColor } from "./tag-color";
+
 export interface IClientStatus {
 	id: string;
 	createdAt: string;
@@ -6,5 +8,7 @@ export interface IClientStatus {
 	default: boolean;
 	weight: number;
 	regionId: string;
+	darkColors?: ITagColor | null;
+	lightColors?: ITagColor;
 	metadata?: any;
 }
