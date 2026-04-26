dd20dc3f6f19d6b23534a5645d6f0f5bf9538ac9
Zahid Allaulddin <zahidalaaudin@gmail.com>
2026-04-26 19:22:30 +0300

    feat: add visibility rules to IClientRequestCategoryField interface

--- dist/types/client-request-category-field.d.ts
+++ dist/types/client-request-category-field.d.ts
@@ -1,5 +1,29 @@
 import { IModel } from "./models";
 
+export interface IClientRequestFieldVisibilityRule {
+	id: string;
+	action: "SHOW" | "HIDE";
+	operator: "EQUALS" | "NOT_EQUALS" | "CONTAINS" | "NOT_CONTAINS" | "IS_EMPTY" | "IS_NOT_EMPTY";
+	sourceFieldId: string;
+	targetFieldId: string;
+	order: number;
+	compareValueString?: string;
+	compareValueNumber?: number;
+	compareValueBoolean?: boolean;
+	compareValueDate?: string;
+	metadata?: any;
+	sourceField?: {
+		id: string;
+		name: string;
+		type?: IClientRequestCategoryField["type"];
+	};
+	targetField?: {
+		id: string;
+		name: string;
+		type?: IClientRequestCategoryField["type"];
+	};
+}
+
 export interface IClientRequestCategoryField {
 	id: string;
 	createdAt: string;
@@ -36,4 +60,7 @@ export interface IClientRequestCategoryField {
 		hasDependency?: boolean;
 		variant?: string;
 	};
+
+	visibilityRulesAsSource?: IClientRequestFieldVisibilityRule[];
+	visibilityRulesAsTarget?: IClientRequestFieldVisibilityRule[];
 }
