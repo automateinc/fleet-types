828e5142cc1bf597cbc1aa1347fd49806977537d
Zahid Allaulddin <zahidalaaudin@gmail.com>
2026-05-05 19:26:54 +0300

    feat: add IClientStatusCheckRequirementCondition and IClientStatusCheckRequirementConditionGroup interfaces

--- dist/types/client-status-check-requirement.d.ts
+++ dist/types/client-status-check-requirement.d.ts
@@ -1,3 +1,29 @@
+export interface IClientStatusCheckRequirementCondition {
+	id?: string;
+	createdAt?: string;
+	updatedAt?: string;
+	order?: number;
+	type?: string;
+	operator?: string;
+	categoryId: string;
+	categoryFieldId?: string;
+	compareValueBoolean?: boolean;
+	compareValueDate?: string;
+	compareValueNumber?: number;
+	compareValueString?: string;
+	metadata?: any;
+}
+
+export interface IClientStatusCheckRequirementConditionGroup {
+	id?: string;
+	createdAt?: string;
+	updatedAt?: string;
+	order?: number;
+	operator?: string;
+	conditions?: IClientStatusCheckRequirementCondition[];
+	metadata?: any;
+}
+
 export interface IClientStatusCheckRequirement {
 	id: string;
 	createdAt: string;
@@ -6,14 +32,18 @@ export interface IClientStatusCheckRequirement {
 	optional: boolean;
 	order: number;
 	tooltip?: string;
+	type?: string;
+	operator?: string;
 
 	statusCheckId: string;
 	categoryId: string;
 
 	categoryFieldId?: string;
-
-	optionalWhenCategoryId?: string;
-	optionalWhenCategoryFieldId?: string;
+	compareValueBoolean?: boolean;
+	compareValueDate?: string;
+	compareValueNumber?: number;
+	compareValueString?: string;
+	optionalConditionGroups?: IClientStatusCheckRequirementConditionGroup[];
 
 	metadata?: any;
 }
